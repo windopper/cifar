93a35b3bc6e6b25bee97b8148943071e26ff8065
windopper <open0916@naver.com>
2025-11-16 21:33:06 +0900

    Add DeepBaselineNetBN3ResidualPreAct model and update README.md performance metrics
    - Included DeepBaselineNetBN3ResidualPreAct in `main.py` to expand model options.
    - Updated `get_net` function to support the new model.
    - Revised performance metrics for 'deep_baseline3_bn_residual_dla_tree' in `README.md` to reflect updated results.

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ from utils.mixup import MixupCollator, MixupCriterion
 from utils.model_name import get_model_name_parts
 from utils.training_config import print_training_configuration
 from models.deep_baseline3_bn_residual import DeepBaselineNetBN3Residual
+from models.deep_baseline3_bn_residual_preact import DeepBaselineNetBN3ResidualPreAct
 from models.deep_baseline3_bn_residual_swish import DeepBaselineNetBN3ResidualSwish
 from models.deep_baseline3_bn_residual_swiglu import DeepBaselineNetBN3ResidualSwiGLU
 from models.deep_baseline3_bn_residual_dla import DeepBaselineNetBN3ResidualDLA
@@ -100,6 +101,7 @@ def get_net(name: str, init_weights: bool = False):
         'deep_baseline2_bn_residual_grn': DeepBaselineNetBN2ResidualGRN(init_weights=init_weights),
         'deep_baseline3_bn': DeepBaselineNetBN3(init_weights=init_weights),
         'deep_baseline3_bn_residual': DeepBaselineNetBN3Residual(init_weights=init_weights),
+        'deep_baseline3_bn_residual_preact': DeepBaselineNetBN3ResidualPreAct(init_weights=init_weights),
         'deep_baseline3_bn_residual_swish': DeepBaselineNetBN3ResidualSwish(init_weights=init_weights),
         'deep_baseline3_bn_residual_swiglu': DeepBaselineNetBN3ResidualSwiGLU(init_weights=init_weights),
         'deep_baseline3_bn_residual_dla': DeepBaselineNetBN3ResidualDLA(init_weights=init_weights),
@@ -235,7 +237,7 @@ def parse_args():
                                  'deep_baseline_bn_dropout_resnet', 'deep_baseline_se', 'resnet18',
                                  'vgg16', 'mobilenetv2', 'densenet121', 'deep_baseline2_bn', 'deep_baseline2_bn_residual',
                                  'deep_baseline2_bn_residual_preact', 'deep_baseline3_bn', 'deep_baseline2_bn_resnext', 'deep_baseline2_bn_residual_se',
-                                 'deep_baseline2_bn_residual_grn', 'deep_baseline3_bn_residual', 'deep_baseline3_bn_residual_swish', 'deep_baseline3_bn_residual_swiglu', 
+                                 'deep_baseline2_bn_residual_grn', 'deep_baseline3_bn_residual', 'deep_baseline3_bn_residual_preact', 'deep_baseline3_bn_residual_swish', 'deep_baseline3_bn_residual_swiglu', 
                                  'deep_baseline3_bn_residual_dla', 'deep_baseline3_bn_residual_dla_tree', 'deep_baseline3_bn_residual_group', 
                                  'deep_baseline3_bn_residual_mish', 'deep_baseline4_bn_residual',
                                  'deep_baseline3_bn_residual_shakedrop',
